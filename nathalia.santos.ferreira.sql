30b1964739f957d6303fc188539ab3104be23191
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2024-11-12 11:19:54 -0300

    feat: Create inserts for the author table

--- nathalia.santos.ferreira.sql
+++ nathalia.santos.ferreira.sql
@@ -5,4 +5,13 @@ CREATE TABLE autores (
     nome_autor VARCHAR(100) NOT NULL
 )
 
+-- Create inserts for the author table --
+-- Criando Inserts para a tabela autor --
+
+INSERT INTO autores (nome_autor) VALUES 
+('Nathalia Santos Ferreira'),
+('Agda Serpentine'),
+('Gabriela Fernanda'),
+('Julia Arruda');
+
 
