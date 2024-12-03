7e41defbcca5d298725ccff9b7db722ae343375b
Alexandra Aversani <158167102+Alexandraaversani@users.noreply.github.com>
2024-12-03 08:27:16 -0300

    feat: criação das queries de simulados

--- alexandra_aversani.sql
+++ alexandra_aversani.sql
@@ -16,6 +16,28 @@ INSERT INTO Simulado (titulo, nivel, link_questoes, duracao) VALUES
 ('Simulado Nível 5', 5, 'https://www.jotform.com/form/243036947508058', '06:00:00'),
 ('Teste suas Capacidades', 0, 'https://cursoenemgratuito.com.br/simulado-geral-de-historia-para-o-enem/', '07:00:00');
 
+
+--Select que retorna todos os simulados de um nível específico--
+
+SELECT * FROM Simulados WHERE nivel = 2;
+
+--Select que conta quantos simulados existem na tabela--
+
+SELECT COUNT(*) AS total_simulados FROM Simulado;
+
+--Select que exibe simulados com duração superior a 3 horas--
+
+SELECT * FROM Simulado WHERE duracao > '03:00:00';
+
+--Delete que remove um simulado específico usando ID--
+
+DELETE FROM Simulado WHERE id = 3;
+
+--Delete que remove todos os simulados com nível igual a 5--
+
+DELETE FROM Simulado WHERE nivel = 5;
+
+
 --Criando o MER da tabela Simulado--
 
 ENTIDADES:
